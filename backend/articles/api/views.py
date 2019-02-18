c9b9f8e262b26d9d0498a8a46a515e31128bc479
Andi Hakim <andi.hakim.arif@gmail.com>
2019-02-18 21:45:56 +0700

    replace genericView with viewSet

--- backend/articles/api/views.py
+++ backend/articles/api/views.py
@@ -1,14 +1,37 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
-
-from articles.models import Article
 from .serializers import ArticleSerializer
+from articles.models import Article
+from rest_framework import viewsets
 
 
-class ArticleListView(ListAPIView):
+class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
+# from rest_framework.generics import (ListAPIView, RetrieveAPIView,
+#                                      CreateAPIView, UpdateAPIView,
+#                                      DestroyAPIView)
 
-class ArticleDetailView(RetrieveAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
+
+# class ArticleListView(ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+
+# class ArticleDetailView(RetrieveAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+
+# class ArticleCreateView(CreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+
+# class ArticleUpdateView(UpdateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+
+# class ArticleDeleteView(DestroyAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
