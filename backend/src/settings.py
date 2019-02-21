720b3f62f78e7b1fb15cc195fa51a4d6a0f765fc
Andi Hakim <andi.hakim.arif@gmail.com>
2019-02-21 23:06:10 +0700

    add restauth

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -37,8 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'rest_auth.registration',
     'corsheaders',
 
     'articles',
@@ -139,3 +146,9 @@ REST_FRAMEWORK = {
 
 # CORS HEADER
 CORS_ORIGIN_ALLOW_ALL = True
+
+# django allauth
+SITE_ID = 1
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_AUTHENTICATION_METHOD = 'username'
+ACCOUNT_EMAIL_REQUIRED = False
