ef4d31fec280ba10769cc1dfd98c45f13a4f44fd
andi hakim arif <andi.hakim.arif@gmail.com>
2019-04-25 11:38:54 +0700

    remove whitenoise for heroku

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -65,8 +65,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
     'corsheaders.middleware.CorsMiddleware',
-    # Simplified static file serving.
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'src.urls'
